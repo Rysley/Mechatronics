1f6a098c7382b0bb6546eaf004957f24a078b347
Rysley <ryszardleykam@gmail.com>
2022-10-21 11:58:40 +0200

    navigation moved to top + test justify in project guide

---
+++ resources/js/markup/courses.js
@@ -0,0 +1,48 @@
+
+const coursesMarkups = {
+    mecha: `
+    <h2 class='highlights__text_element'>MECHATRONICS</h2>
+    <p class='highlights__text_element'>Mechatronics (the word comes from the combination of the terms mechanics and electronics) is a field of science that integrates mechanics, electronics, automation, computer science, and other technical and non-technical areas. It integrates some of the more difficult sciences, which is why it is becoming more popular in the labor market.
+    </p>
+      <p class='highlights__text_element'>Thanks to its reliable features and simplicity of use, mechatronics has significantly relieved humans of many difficult tasks</p>
+      
+      <h5 class='highlights__text_element'>Mechatronic is something that we are facing in everyday life,
+      thanks to it was created among other:</h5>
+      <ul>
+      <li><p class='highlights__text_element'>robots, for example robot ,,da Vinci” – used in medicine</p></li>
+      
+      <li><p class='highlights__text_element'>automatically opening doors</p></li>
+      
+      <li><p class='highlights__text_element'>automatic barrier (at the entrance to the parking, at rail crossings)</p></li>
+      
+      <li><p class='highlights__text_element'>traffic lights</p></li>
+     `,
+    elraCAN: `<h2 class='highlights__text_element'>ELRASOFT – CAN</h2>
+    <p class='highlights__text_element'>CAN bus analyzer (Elrasoft)</p>
+  
+    <p class='highlights__text_element'>This training has to show how look like transering information in CAN, how systems respond to and answer questions about various physical quantities and how they and how they answer them.</p>
+    
+    <p class='highlights__text_element'>Our task is to present an environment for creating scripts in Visual Pascal language, the knowledge of which helps in reading data transmitted on CAN bus.</p>
+    <a class="course__button button" target="_blank" rel="noopener noreferrer" href="http://quality-education.eu/courses/"><span>Sign Up</span></a>`,
+    elraProg: `<h2 class='highlights__text_element'>ELRASOFT – PROGRAMMER</h2>
+  
+    <p class='highlights__text_element'>Device and memory programmer – Elrasoft</p>
+  
+  <p class='highlights__text_element'>Currently, all mechatronic modules are built on processors, which can collect information in real-time and respond to external commands. During this classes will be discussed about different ways of reading the collected information using the Elrasoft programmer. This programmer has a built-in environment for writing scripts in Visual Pascal that support the work. Our task (during these classes) will be to combine the skiils to read data with learning programming.</p>
+  <a class='course__button button' target="_blank" rel="noopener noreferrer" href='http://quality-education.eu/courses/'><span>Sign Up</span></a>`,
+    arduino_1: `<h2 class='highlights__text_element'>ARDUINO COURSE I</h2>
+    <p class='highlights__text_element'>Arduino – measuring and digitizing various physical objects.</p>
+  
+    <p class='highlights__text_element'>In this course, using the Arduino module and a specially prepared kit for measuring physical values (voltage, current, vibration, temperature, humidity, time, motion sensor, distance, tilt, magnetic switch, magnetic field sensors, infrared receiver) the student learns how to convert these physical quantities into a language understood by the components and integrated circuits.</p>
+    <a class='course__button button' target="_blank" rel="noopener noreferrer" href='http://quality-education.eu/courses/'><span>Sign Up</span></a>`,
+    arduino_2: `<h2 class='highlights__text_element'>ARDUINO COURSE II</h2>
+     <p class='highlights__text_element'>Arduino – read and write numerical data from integrated circuits</p>
+  
+    <p class='highlights__text_element'>In this course, the student will expand their knowledge about the transfer of information to integrated circuits, whose task is to collect and store results, using i2c, spi and microwire buses.</p>
+    <a class='course__button button' target="_blank" rel="noopener noreferrer" href='http://quality-education.eu/courses/'><span>Sign Up</span></a>`,
+    arduino_3: `<h2 class='highlights__text_element'>ARDUINO COURSE III</h2>
+    <p class='highlights__text_element'>Arduino – CAN network</p>
+  
+    <p class='highlights__text_element'>This course is designed for those who want to learn the secrets related to the transmission of information using CAN bus. CAN bus was created to exchange information between modules in cars, streetcars, elevators. The class will describe and explain how to construct appropriate questions asking about basic physical quantities (speed, acceleration, temperature, pressure)</p>
+    <a class='course__button button' target="_blank" rel="noopener noreferrer" href='http://quality-education.eu/courses/'><span>Sign Up</span></a>`,
+  };
